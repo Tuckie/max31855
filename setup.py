9ee0793b468f397a1613a30a236133b811bbe623
David Leigh <dleigh@us.ibm.com>
2017-03-12 14:54:15 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='max31855',
+    version='2.0',
+    description='Python driver for MAX31855 Cold-Junction Compensated Thermocouple-to-Digital Converter',
+    url='https://github.com/Tuckie/max31855',
+    author='Tuckie',
+    packages=find_packages(),
+    install_requires=[]
+)
